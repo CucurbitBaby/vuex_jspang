bbb5e5c18e34c2ca0cb2bceae210321a0b4b367d
cucurbitbaby <cucurbitbaby@163.com>
2019-08-20 03:47:02 +0800

    second commit vuex-mapState

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -2,7 +2,7 @@
 	<div>
 		<h2>{{ msg }}</h2>
 		<hr />
-		<h3>{{ $store.state.count }}</h3>
+		<h3>{{ $store.state.count }} ----  {{ count }}</h3>
 
 		<p>
 			<button @click="$store.commit('add')">+</button>
@@ -15,15 +15,37 @@
 
 <script>
 	
-import store from '@/vuex/store'		
+import store from '@/vuex/store'	
+import { mapState } from 'vuex'	
 export default { 
 	data(){
 		return {
 			msg: 'Hello Vuex'
 		}
 	},
+	computed:mapState(['count']),
 	store
 }
+
+
+/*
+3种方式实现：{{ $store.state.count }}直接用 {{ count }}
+1.	computed:{
+		count(){
+			return this.$store.state.count
+		}
+	},
+2.	import { mapState } from 'vuex'	
+	computed:mapState({
+		count:state=>state.count
+	}),
+	store
+3.	import { mapState } from 'vuex'		// 推荐这种方式	
+	computed:mapState(['count']]),
+	store
+
+*/
+
 
 </script>
 
